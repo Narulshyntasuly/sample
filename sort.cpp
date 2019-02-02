6c34026f3981ffec638876e7a46cfb8676cc3037
Narulshyntasuly <43246835+Narulshyntasuly@users.noreply.github.com>
2019-02-02 12:14:55 +0600

    kjk
    jijii

---
+++ sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main ()
+{
+string s,t;
+cin>>s,t;
+
+cout<<"yes";}
+else cout<<"No"
+
+
+
+
+    return 0;
+}
